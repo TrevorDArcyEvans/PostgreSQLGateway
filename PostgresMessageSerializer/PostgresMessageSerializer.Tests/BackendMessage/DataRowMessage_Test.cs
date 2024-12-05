c370fce285f5229daf1d32668b211c1fd8df8a40
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-05 19:15:52 +0000

    - WIP more de/serialisation tests

---
+++ PostgresMessageSerializer/PostgresMessageSerializer.Tests/BackendMessage/DataRowMessage_Test.cs
@@ -0,0 +1,39 @@
+namespace PostgresMessageSerializer.Tests;
+
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+public class DataRowMessage_Test
+{
+  [Fact]
+  public void Serialize_Deserialize_roundtrip()
+  {
+    // arrange
+    var sut = new DataRowMessage();
+    sut.ColumnCount = 3;
+    for (var i = 1; i <= sut.ColumnCount; i++)
+    {
+      var rowField = new RowField();
+      rowField.Length = 2 * i;
+
+      var rowValueBytes = new List<byte>(rowField.Length);
+      for (var j = 0; j < rowField.Length; j++)
+      {
+        rowValueBytes.Add((byte)(7 * j));
+      }
+
+      rowField.Value = rowValueBytes;
+
+      sut.Rows.Add(rowField);
+    }
+
+    // act
+    var data = sut.Serialize();
+    var result = new DataRowMessage();
+    result.Deserialize(data);
+
+    // assert
+    result.Should().BeEquivalentTo(sut);
+  }
+}
--- PostgresMessageSerializer/PostgresMessageSerializer/BackendMessage/DataRowMessage.cs
+++ PostgresMessageSerializer/PostgresMessageSerializer/BackendMessage/DataRowMessage.cs
@@ -7,20 +7,33 @@ public class DataRowMessage : BackendMessage
   public static byte MessageTypeId = (byte)'D';
 
   public short ColumnCount { get; set; }
-  public IList<RowField> Rows { get; set; }
+  public IList<RowField> Rows { get; } = new List<RowField>();
 
   public override byte[] Serialize()
   {
-    throw new System.NotImplementedException();
+    using var buffer = new PostgresProtocolStream();
+
+    buffer.Write(ColumnCount);
+
+    for (var i = 0; i < ColumnCount; i++)
+    {
+      var rowField = Rows[i];
+      buffer.Write(rowField.Length);
+
+      for (var j = 0; j < rowField.Length; j++)
+      {
+        buffer.Write(rowField.Value[j]);
+      }
+    }
+
+    return buffer.ToArray();
   }
 
   public override void Deserialize(byte[] payload)
   {
-    var buffer = new PostgresProtocolStream(payload);
+    using var buffer = new PostgresProtocolStream(payload);
 
     ColumnCount = buffer.ReadInt16();
-
-    Rows = new List<RowField>();
 
     for (var i = 0; i < ColumnCount; i++)
     {
