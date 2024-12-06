dac260630888f4cbffd91ce7b6a13524608d47a2
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-06 14:13:15 +0000

    - initialise to safe value

--- PostgresMessageSerializer/PostgresMessageSerializer/FrontendMessage/Parameter.cs
+++ PostgresMessageSerializer/PostgresMessageSerializer/FrontendMessage/Parameter.cs
@@ -2,13 +2,7 @@ namespace PostgresMessageSerializer;
 
 public class Parameter
 {
-  public int Length
-  {
-    get
-    {
-      return Value.Length;
-    }
-  }
+  public int Length => Value.Length;
 
-  public byte[] Value { get; set; }
+  public byte[] Value { get; set; } = [];
 }
