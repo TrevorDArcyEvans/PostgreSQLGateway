1dd858d161385f59bf758952e7be23e058396d80
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-08 16:21:21 +0000

    - Npgsql CLI test harness

---
+++ PostgreSQLGateway.Test.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using Npgsql;
+
+var connectionString = "Host=localhost;Username=mylogin;Password=mypass;Database=mydatabase";
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+await using var command = dataSource.CreateCommand("SELECT some_field FROM some_table");
+await using var reader = await command.ExecuteReaderAsync();
+
+while (await reader.ReadAsync())
+{
+  Console.WriteLine(reader.GetString(0));
+}
