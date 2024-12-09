684724a52fbacecc754126d7dc842efa16aa6aac
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-09 15:14:10 +0000

    - bump PostgreSQL version to latest

--- PostgreSQLGateway/Program.cs
+++ PostgreSQLGateway/Program.cs
@@ -155,7 +155,7 @@ internal class Program
               // parameter statuses
               stream.Write(Serializer.Serialize(new ParameterStatusMessage("application_name", "PostgreSQLGateway")));
               stream.Write(Serializer.Serialize(new ParameterStatusMessage("client_encoding", "UTF8")));
-              stream.Write(Serializer.Serialize(new ParameterStatusMessage("server_version", "14.7")));
+              stream.Write(Serializer.Serialize(new ParameterStatusMessage("server_version", "17.2")));
               stream.Write(Serializer.Serialize(new ParameterStatusMessage("server_encoding", "UTF8")));
               stream.Write(Serializer.Serialize(new ParameterStatusMessage("DateStyle", "ISO")));
               stream.Write(Serializer.Serialize(new ParameterStatusMessage("PreferQueryMode", "simple")));
