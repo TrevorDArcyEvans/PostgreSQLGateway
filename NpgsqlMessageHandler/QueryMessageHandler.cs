3733cf1194f729e55d9be29032456cce39073407
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-10 16:36:06 +0000

    - move Npgsql to separate assy

--- NpgsqlMessageHandler/QueryMessageHandler.cs
+++ NpgsqlMessageHandler/QueryMessageHandler.cs
@@ -1,9 +1,8 @@
-using PostgreSQLGateway.Interfaces;
-
-namespace PostgreSQLGateway.Npgsql;
+namespace NpgsqlMessageHandler;
 
 using System.Net.Sockets;
 using PostgresMessageSerializer;
+using PostgreSQLGateway.Interfaces;
 
 public class QueryMessageHandler : IMessageHandler<QueryMessage>
 {
