a83851a80cbaf77b66ab80413f5a5c0683069f86
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-12-10 15:24:50 +0000

    - sort usings

--- PostgreSQLGateway/JDBCMetadata/ParseMessageHandler.cs
+++ PostgreSQLGateway/JDBCMetadata/ParseMessageHandler.cs
@@ -1,9 +1,8 @@
-using PostgreSQLGateway.Interfaces;
-
 namespace PostgreSQLGateway.JDBCMetadata;
 
 using System.Net.Sockets;
 using PostgresMessageSerializer;
+using PostgreSQLGateway.Interfaces;
 
 public class ParseMessageHandler: IMessageHandler<ParseMessage>
 {
